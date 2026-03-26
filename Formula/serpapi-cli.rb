71fe97a70a2129aca2fcf7678484626338981690
ilyazub <ilya@serpapi.com>
2026-03-26 15:10:02 +0100

    feat: add serpapi-cli v0.1.0 with real SHA256 hashes

--- Formula/serpapi-cli.rb
+++ Formula/serpapi-cli.rb
@@ -7,22 +7,22 @@ class SerpapiCli < Formula
   on_macos do
     on_arm do
       url "https://github.com/serpapi/serpapi-cli/releases/download/v#{version}/serpapi-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "FILL_IN_AFTER_FIRST_RELEASE"
+      sha256 "4e5447dcba96a7bcae51cb3c872159e982311f649e7229f64b735307db0a57f2"
     end
     on_intel do
       url "https://github.com/serpapi/serpapi-cli/releases/download/v#{version}/serpapi-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "FILL_IN_AFTER_FIRST_RELEASE"
+      sha256 "daafa551c0d0edbd4540f587f150b397ce03d44fea7ff58974828f983efaa19c"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/serpapi/serpapi-cli/releases/download/v#{version}/serpapi-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "FILL_IN_AFTER_FIRST_RELEASE"
+      sha256 "70fdd1b0d4e584959136b4544583b066aac1cd8e6f078ccc24ee0d61f6ac000e"
     end
     on_intel do
       url "https://github.com/serpapi/serpapi-cli/releases/download/v#{version}/serpapi-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "FILL_IN_AFTER_FIRST_RELEASE"
+      sha256 "23e49187d7478357db9ccb3bd2cca05b9961a74fd5b2ba6379798314ab88f02f"
     end
   end
 
