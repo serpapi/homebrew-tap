8f3bea25298fdaa657531905baacd8f57aad05a6
ilyazub <ilya@serpapi.com>
2026-03-28 00:02:16 +0100

    Switch to Go build for serpapi-cli

--- Formula/serpapi-cli.rb
+++ Formula/serpapi-cli.rb
@@ -6,13 +6,10 @@ class SerpapiCli < Formula
   license "MIT"
   head "https://github.com/serpapi/serpapi-cli.git", branch: "main"
 
-  depends_on "rust" => :build
-  depends_on "openssl@3"
+  depends_on "go" => :build
 
   def install
-    ENV["OPENSSL_NO_VENDOR"] = "1"
-    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
-    system "cargo", "install", *std_cargo_args
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/serpapi"
   end
 
   test do
@@ -22,6 +19,6 @@ class SerpapiCli < Formula
     assert_match "Invalid API key", shell_output("#{bin}/serpapi --api-key invalid account 2>&1", 1)
     assert_match "canonical_name", shell_output("#{bin}/serpapi locations q=Austin")
     assert_match "No API key found", shell_output("#{bin}/serpapi archive abc123 2>&1", 2)
-    assert_match "Invalid API key", shell_output("#{bin}/serpapi --api-key invalid archive abc123 2>&1", 1)
+    assert_match "api_error", shell_output("#{bin}/serpapi --api-key invalid archive abc123 2>&1", 1)
   end
 end
